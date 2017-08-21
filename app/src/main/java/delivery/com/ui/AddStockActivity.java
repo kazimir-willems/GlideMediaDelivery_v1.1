205e7d35dc94265a35bc30c5ff79859dfaacfa8a
kazimir-willems <kazimirwillems@outlook.com>
2017-08-21 07:01:14 +0100

    - Add Stock Activity

---
+++ app/src/main/java/delivery/com/ui/AddStockActivity.java
@@ -0,0 +1,15 @@
+package delivery.com.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import delivery.com.R;
+
+public class AddStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_stock);
+    }
+}
